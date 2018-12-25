4f84feab845ab5fc55a6abdf9e322431cb6d534b
sailesh97 <33670045+sailesh97@users.noreply.github.com>
2018-12-25 20:59:33 +0530

    Updates

---
+++ precache-manifest.037fb6f50528190a97331d70015b0785.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3fb466f4a13d72511df",
+    "url": "/Lyrics_Finder_React_App/static/js/runtime~main.b3fb466f.js"
+  },
+  {
+    "revision": "fc849b8d62d5d7aecc32",
+    "url": "/Lyrics_Finder_React_App/static/js/main.fc849b8d.chunk.js"
+  },
+  {
+    "revision": "b24910eddd1c26c0ed9d",
+    "url": "/Lyrics_Finder_React_App/static/js/1.b24910ed.chunk.js"
+  },
+  {
+    "revision": "9724ba085228f782cb433913cb9641b4",
+    "url": "/Lyrics_Finder_React_App/index.html"
+  }
+];
